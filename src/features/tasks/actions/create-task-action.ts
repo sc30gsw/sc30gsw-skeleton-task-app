f5d1681d7113176cd81baca7313d744dd6df188f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-08-08 16:55:06 +0900

    revalidateTagを有効化し、タスク作成後にキャッシュを更新する

--- src/features/tasks/actions/create-task-action.ts
+++ src/features/tasks/actions/create-task-action.ts
@@ -2,6 +2,8 @@
 
 import type { SubmissionResult } from '@conform-to/react'
 import { parseWithZod } from '@conform-to/zod/v4'
+import { revalidateTag } from 'next/cache'
+import { FETCH_ALL_TASKS_CACHE_KEY } from '~/constants/cache-keys'
 import { TASK_ERROR_MESSAGES } from '~/features/tasks/constants/validation'
 
 import { TaskService } from '~/features/tasks/services/task-service'
@@ -15,10 +17,11 @@ export async function createTaskAction(_: unknown, formData: FormData) {
   if (submission.status !== 'success') {
     return submission.reply()
   }
+
   try {
     taskService.createTask(submission.value.title)
 
-    // revalidateTag(FETCH_ALL_TASKS_CACHE_KEY)
+    revalidateTag(FETCH_ALL_TASKS_CACHE_KEY)
     return submission.reply()
   } catch (_) {
     return {
